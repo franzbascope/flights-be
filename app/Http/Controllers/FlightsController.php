8a20d01fdfbe17375df09418ce6bf391a93114de
Franz Bascope <fbascope@credibl.com>
2022-07-30 07:58:29 -0400

    mock request index for flights controller

--- app/Http/Controllers/FlightsController.php
+++ app/Http/Controllers/FlightsController.php
@@ -6,19 +6,23 @@ use Illuminate\Http\Request;
 
 class FlightsController extends Controller
 {
-    public function index(){
-        $flights = array($this->store(),$this->store());
+    public function index()
+    {
+        $flights = [];
+        for($i = 0; $i < random_int(5,20);$i++){
+            $flights[] = $this->store();
+        }
         return $flights;
     }
 
-    public function store(){
+    public function store()
+    {
         return [ "uuid" => uniqid(),
-            "flight_number" => mb_substr(0,5) ,
+            "flight_number" => mb_substr(0, 5) ,
             "startDateTime" => new \DateTime(),
             "endDateTime" => new \DateTime(),
             "crew" => [],
             "airplane" => []
         ];
     }
-
 }
